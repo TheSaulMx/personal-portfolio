99b56be18d2550c1a0023e73ca882937aabb2c3f
TheSaulMx <saaulmx@gmail.com>
2025-06-04 16:48:24 -0600

    feat: rename the file

--- src/components/icons/PortfolioIcon.jsx
+++ src/components/icons/PortfolioIcon.jsx
@@ -10,10 +10,10 @@ export default class PortfolioIcon extends Component {
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="icon icon-tabler icons-tabler-outline icon-tabler-briefcase-2"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="icon icon-tabler icons-tabler-outline icon-tabler-briefcase-2"
       >
         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
         <path d="M3 9a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-9z" />
